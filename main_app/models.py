7245a1e6bc37e2e8be6d49bffc1c5f22c93af735
erikacolette <erikatidwell.c@gmail.com>
2021-02-05 18:41:43 -0500

    supply name changes

--- main_app/models.py
+++ main_app/models.py
@@ -54,7 +54,7 @@ class Photo(models.Model):
 class Addon(models.Model):
   name = models.CharField(max_length=50)
   color = models.CharField(max_length=20)
-  description = models.CharField(max_length=20)
+  description = models.CharField(max_length=100)
 
   def __str__(self):
     return self.name
