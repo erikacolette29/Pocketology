c415a63d6f9410054f210ad477963243a1c9b614
erikacolette <erikatidwell.c@gmail.com>
2021-02-06 12:47:21 -0500

    photo herbs working

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Toxic, Photo, Rating, Herb, Addon
+from .models import Toxic, Photo, Rating, Herb, Addon, HerbPhoto
 from .forms import RatingForm
 import uuid
 import boto3
@@ -135,7 +135,27 @@ def add_photo(request, toxic_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', toxic_id=toxic_id)
+    
 
+
+def addherb_photo(request, herb_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to cat_id or cat (if you have a cat object)
+            photo = HerbPhoto(url=url, herb_id=herb_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail_herbs', herb_id=herb_id)
 
 
 
