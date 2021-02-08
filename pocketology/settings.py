1a31ce0142dae9a3b2a5667912512eee2af65e7a
erikacolette <erikatidwell.c@gmail.com>
2021-02-08 16:33:39 -0500

    Config deployment

--- pocketology/settings.py
+++ pocketology/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -132,3 +132,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 LOGOUT_REDIRECT_URL = '/'
+
+
+import django_on_heroku
+django_on_heroku.settings(locals())
